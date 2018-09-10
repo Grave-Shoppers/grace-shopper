116a48265e3e8c4432ab16b08d2b4f007b26abb3
Michelle Messenger <michelle.messenger719@gmail.com>
2018-09-10 17:41:36 -0400

    fixes errors

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,18 +1,23 @@
-const User = require('./user')
+const db = require('../db')
+const Sequelize = require('sequelize')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+// register models
+const { Product, Review, User } = require('./models')
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
-module.exports = {
-  User
-}
+const Purchases = db.define('purchases', {
+  status: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  }
+})
+
+Review.belongsTo(Product)
+Product.hasMany(Review)
+
+Review.belongsTo(User)
+User.hasMany(Review)
+
+Product.belongsToMany(User, { through: Purchases })
+User.belongsToMany(Product, { through: Purchases })
+
+module.exports = db
