5df6afdf16c1a5cd51529da00d4c2de2c0bc7774
Irene Hakes <irenebrbr@gmail.com>
2018-09-12 16:19:28 -0400

    cart component renders

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,47 +1,48 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { CartProducts } from '../server/db/models'
-import getCart from '../store/productReducer'
+// import { CartProducts } from '../server/db/models'
+import { getCart } from '../store/productReducer'
 
-// const mapStateToProps = (state) => ({
-//   products: state.products
-// })
+const mapStateToProps = (state) => ({
+  products: state.products,
+  loaded: state.loaded
+})
 
-// const mapDispatchToProps = (dispatch) => ({
-//   getInitialProducts: () => {
-//     dispatch(getCart)
-//   }
-// })
+const mapDispatchToProps = (dispatch) => ({
+  getInitialProducts: () => {
+    dispatch(getCart)
+  }
+})
 
 class Cart extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      products: [{
-    name: "Wonder Woman",
-    price: 45.99,
-    description: "Yay",
-    quantity: 50,
-    category: "Costume"
-  }]
+      products: []
     }
   }
 
   componentDidMount() {
-
+    console.log('our component mounted!')
+    this.props.getInitialProducts()
   }
 
   render() {
     console.log(this.state)
     return (
-    <div>
-      {
-        this.state.products.map(product => {
-          return (<div key={product.id}>{product.name}</div>)})
-      }
-    </div>
+      this.props.loaded
+        ? (
+          <div>
+            {
+              this.props.products.map(product => {
+                return (<div key={product.id}>{product.name}</div>)
+              })
+            }
+          </div>
+        )
+        : <div>Loading...</div>
     )
   }
 }
 
-export default Cart
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)
