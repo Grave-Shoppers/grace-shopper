c8f15ff591acd653dadb8ed58f8446884b000b8b
Irene Hakes <irenebrbr@gmail.com>
2018-09-19 10:49:18 -0400

    pulled michelles down

--- client/components/category.js
+++ client/components/category.js
@@ -1,45 +1,3 @@
-// 'use strict'
-// import React from 'React'
-// import { connect } from 'react-redux'
-
-// export const Category = (props) => {
-//   const category = props.match.params.category
-
-//   const selectedCategory = props.products.products.filter((product) =>
-//   product.category === category)
-
-//   if(selectedCategory){
-//     return (
-//       <div className="wrapper">
-//       <div className="column">
-//       <h1>{category}</h1>
-//       <div className="parent">
-//       {
-//        selectedCategory.map(product => (
-//         <div className="child" key={product.id}>
-
-// 							<a className="child" href={`/products/${product.id}`}>
-// 								<img src={product.imageUrl} width="100" height="100" />
-// 							</a>
-// 							<div> {product.name} </div>
-// 							<div>Price: ${product.price}</div>
-// 							<div>Description: {product.description}</div>
-//               </div>
-
-//         ))
-//       }
-//   </div>
-//     </div>
-//       </div>
-//     )
-// }
-// }
-
-// const mapStateToProps = state => ({
-//   products: state.products
-// })
-
-// export default connect(mapStateToProps)(Category)
 'use strict'
 import React, { Component } from 'React'
 import { connect } from 'react-redux'
@@ -98,7 +56,7 @@ class Category extends Component {
                     <img src={product.imageUrl} width="100" height="100" />
                   </a>
                   <div> {product.name} </div>
-                  <div>Price: ${product.price}</div>
+                  <div>Price: ${Math.floor(product.price) / 100}</div>
                   <div>Description: {product.description}</div>
                   <button
                     type="button"
